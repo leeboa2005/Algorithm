c6114c924b87337ffe18816cff5bdb124b9b6c86
leeboa2005 <71476841+leeboa2005@users.noreply.github.com>
2024-05-18 14:15:49 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -0,0 +1,14 @@
+let answer = '';
+let newArr = [];
+
+function solution(s) {
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === s[i].toUpperCase()) newArr.push(s[i]);
+        else newArr.push(s[i]);
+    }
+    newArr.sort();
+    newArr.reverse();
+    answer = newArr.join('');
+
+    return answer;
+}

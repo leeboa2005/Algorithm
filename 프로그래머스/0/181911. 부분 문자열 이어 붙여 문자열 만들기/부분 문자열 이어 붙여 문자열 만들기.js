7f2ba675f62dd629fd629d9b68f69f4888facf41
leeboa2005 <71476841+leeboa2005@users.noreply.github.com>
2024-05-27 09:56:44 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
@@ -0,0 +1,11 @@
+function solution(my_strings, parts) {
+    // slice를 사용하여 문자열을 잘라야하는데 
+    // 모든 배열의 값이 반복되야함 
+    
+    let answer = '';
+    for(let i=0; i < my_strings.length; i++){        
+      answer = answer + my_strings[i].slice(parts[i][0],parts[i][1] + 1);
+    }
+    
+    return answer;
+}

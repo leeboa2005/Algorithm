233ec09ccd0edee7cbc8fdab6b2635c45515a68c
leeboa2005 <71476841+leeboa2005@users.noreply.github.com>
2024-09-22 19:49:17 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -16,12 +16,3 @@ let slicedArray = array.slice(i -1 ,j);
     
     return answer;
 }
-
- solution(
-        [1, 5, 2, 6, 3, 7, 4],
-        [
-            [2, 5, 3],
-            [4, 4, 1],
-            [1, 7, 3],
-        ]
-    );

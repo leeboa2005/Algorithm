94e2cacc8bd0ed95075bfbe6828bbddf46668f3a
leeboa2005 <71476841+leeboa2005@users.noreply.github.com>
2024-09-23 23:24:23 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+    let answer = [];
+    for(i=0; i < numbers.length; i++){
+        for(j= i + 1; j < numbers.length; j++){
+        let sum = numbers[i] + numbers[j]
+        if (!answer.includes(sum)){
+            answer.push(sum)
+          }
+       } 
+    }
+    return answer.sort((a, b) => a - b);
+}
